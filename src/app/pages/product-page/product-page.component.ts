a70b03e8637a41ed097e1e19cf94fee6c5e4f2ae
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-21 10:30:11 -0600

    updated quantity select input validation, updated product page structure, minor changes, formatting

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -13,24 +13,32 @@ import { ApiService } from 'src/app/services/api/api.service';
 })
 export class ProductPageComponent implements OnInit {
 	public product : Product = <Product> {};
-	public quantityInput: number = 1;
+	public quantityInput : number = 1;
 		
 	constructor (private activatedRoute: ActivatedRoute, private apiService : ApiService) {}
-
+	
+	onQuantityInput = (event : any) : void => {
+		//todo allow backspacing but when unfocus set to 1 if still blank
+		//todo quantityInput should be updated automatically
+		
+		event.target.value = Math.max (1, event.target.value);
+		
+		this.quantityInput = event.target.value;
+	}
+	
+	addToCart = (quantityInput : number) : void => {
+		console.log (quantityInput, this.product.id);
+		//will take in product id and quantity added
+		//post request to cart
+		//returns message to user, items added to cart
+		// if successful, will show method
+	}
+	
 	ngOnInit () : void {
 		this.activatedRoute.params.subscribe (paramaters => {
 			this.apiService.getProduct (parseInt (paramaters ["productId"]), (body : any) : void => {
 				this.product = body.data;
 			});
 		});
-	}
-
-	addToCart(quantityInput: number){
-		console.log(quantityInput, this.product.id);
-		//will take in product id and quantity added
-		//post request to cart
-		//returns message to user, items added to cart
-		// if successful, will show method
-		
 	}
 }
