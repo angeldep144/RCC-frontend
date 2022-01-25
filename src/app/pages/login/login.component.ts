e5d83dfca0822fb8f8460d0b048d717d49581756
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-01-24 21:59:24 -0600

    added border to theme-color-3 elements in light mode, optimized styles, updated global styles, updated cart page html and styles, updated checkout page html and styles, updated login html, styles, and code, formatting

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,26 +6,32 @@ import { DataService } from 'src/app/services/data/data.service';
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.css']
+	styleUrls: ['./login.component.css'],
+	host: {
+		class: "page flexColumnTop extraLargeGap"
+	}
 })
 export class LoginComponent implements OnInit {
-	errMsg: string = "";
-	identifier: string = "";
-	passwordInput: string = "";
+	public identifierInput : string = "";
+	public passwordInput : string = "";
+	
+	public errorMessage : string = "";
 
-	constructor(private apiServ: ApiService, private dataService: DataService, private router: Router) { }
+	constructor (private apiServ: ApiService, private dataService: DataService) {}
 	
-	goToReg() {
-		this.router.navigate(["/register"])
-	}
+	onKeyDown = (event : KeyboardEvent) : void => {
+		if (event.key === "Enter") {
+			this.login ();
+		}
+	};
 	
-	loginUser(){
-		this.apiServ.createSession(this.identifier,this.passwordInput, (body : any) : void => {
+	login = () : void => {
+		this.apiServ.createSession (this.identifierInput, this.passwordInput, (body : any) : void => {
 			this.dataService.updateUser (body.data);
-		}, (body : any)=>{
-			this.errMsg = "Wrong username or password";
+		}, (body : any) => {
+			this.errorMessage = body.message;
 		});
-	}
+	};
 	
 	ngOnInit () {}
 }
