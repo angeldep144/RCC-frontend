ef38666c9ca0eb6c4f44f169c7814942db1c2965
bradywestveer-revature <93550118+bradywestveer-revature@users.noreply.github.com>
2022-02-01 09:10:20 -0600

    updated backend port to 81

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://ec2-34-201-172-201.compute-1.amazonaws.com:8080/"
+  apiBaseUrl: "http://ec2-34-201-172-201.compute-1.amazonaws.com:81/"
 };
